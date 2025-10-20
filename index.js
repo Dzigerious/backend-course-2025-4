9458f84ddbb34958a95ece09c68a7135b5f3faf7
Захарій bc2025-4 <zakhar.struminskiyprog@gmail.com>
2025-10-20 11:46:41 +0300

    Updated all files, project is working perfectly

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { program } = require("commander");
-const http = require('http');
-const fs = require('fs');
-
+const http = require("http");
+const fs = require("fs").promises;
+const { URL } = require("url");
+const { XMLBuilder } = require("fast-xml-parser");
 
 program
-    .option('-i, --input <path>', 'Input JSON file')
-    .option('-h, --host <host>', 'Input the host ')
-    .option('-p, --port <number>', 'Input server port'); //can use requiredOption
+  .option("-i, --input <path>", "Input JSON file")
+  .option("-h, --host <host>", "Input the host")
+  .option("-p, --port <number>", "Input server port");
 
 program.parse(process.argv);
 const options = program.opts();
@@ -16,32 +17,83 @@ if (!options.input) {
   process.exit(1);
 }
 
-if (!fs.existsSync(options.input)) {
-  console.error("Cannot find input file");
-  process.exit(1);
-}
+fs.access(options.input)
+  .then(() => {
+    if (!options.host) {
+      console.error("Please, specify correct host");
+      process.exit(1);
+    }
 
-if (!options.host) {
-  console.error("Please, specify correct host");
-  process.exit(1);
-}
+    const port = parseInt(options.port, 10);
+    if (Number.isNaN(port)) {
+      console.error("Please specify correct -port");
+      process.exit(1);
+    }
 
-const port = parseInt(options.port, 10);
+    const server = http.createServer((req, res) => {
+      const url = new URL(req.url, `http://${options.host}:${options.port}`);
+      const furnished = url.searchParams.get("furnished");
+      const maxPrice = url.searchParams.get("max_price");
 
-if (Number.isNaN(port)) {
-    console.error("Please specify correct -port");
+      fs.readFile(options.input, { encoding: "utf8" })
+        .then((data) => {
+          const jsonData = JSON.parse(data);
+
+          let filteredData = jsonData;
+          if (furnished === "true") {
+            filteredData = filteredData.filter(
+              (h) => h.furnishingstatus === "furnished"
+            );
+          } else if (furnished === "false") {
+            filteredData = filteredData.filter(
+              (h) => h.furnishingstatus === "unfurnished"
+            );
+          }
+
+          if (maxPrice) {
+            filteredData = filteredData.filter(
+              (h) => Number(h.price) <= Number(maxPrice)
+            );
+          }
+
+          const builder = new XMLBuilder({
+            ignoreAttributes: false,
+            format: true,
+          });
+
+          const xmlData = {
+            houses: {
+              house: filteredData.map((h) => ({
+                price: h.price,
+                area: h.area,
+                furnishingstatus: h.furnishingstatus,
+              })),
+            },
+          };
+
+          const xml = builder.build(xmlData);
+
+          res.writeHead(200, { "Content-Type": "application/xml; charset=utf-8" });
+          res.end(xml);
+        })
+        .catch((err) => {
+          const message =
+            err.code === "ENOENT"
+              ? "Файл не знайдено"
+              : err.name === "SyntaxError"
+              ? "Помилка розбору JSON"
+              : "Помилка читання файлу";
+
+          res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+          res.end(`Internal Server Error: ${message}`);
+        });
+    });
+
+    server.listen(port, options.host, () => {
+      console.log(`Сервер працює на адресі: http://${options.host}:${options.port}`);
+    });
+  })
+  .catch(() => {
+    console.error("Cannot find input file");
     process.exit(1);
-}
-
-const server = http.createServer((req, res) => { //can send inf to client cuz of res
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Сервер працює");
-});
-
-server.listen(port, options.host, () => {
-    console.log(` сервер працює на адресі: http://${options.host}:${options.port}`);
-});
-
-let data = JSON.parse(fs.readFileSync(options.input, 'utf-8'));
-
-
+  });
